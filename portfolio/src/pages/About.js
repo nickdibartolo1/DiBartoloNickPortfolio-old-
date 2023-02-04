98bd79cdaf16bb0caf5143eacb2d588f35dfe6c3
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2023-02-03 21:06:23 -0500

    added all files and installations from old portfolio repo

---
+++ portfolio/src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AboutComponent from '../components/AboutComponent'
+
+export default function About() {
+  return (
+    <div>
+    <AboutComponent></AboutComponent>
+    </div>
+  )
+}
